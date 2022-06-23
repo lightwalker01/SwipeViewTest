b353199930a14eaa6beb6a446e246543ce825a40
lightwalker01 <lightwalker01@users.noreply.github.com>
2022-06-23 09:41:08 +0200

    Add files via upload

---
+++ PianistApp/PianistApp/PianistApp.Android/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Views;
+using System.Threading.Tasks;
+
+namespace PianistApp.Droid
+{
+    [Activity(Label = "PianistApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironment_UnhandledExceptionRaiser;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        #region Exceptions Handling
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            var ex = unhandledExceptionEventArgs.ExceptionObject as Exception;
+            Console.WriteLine($"ERROR Activity caught CurrentDomainOnUnhandledException: {ex}");
+
+        }
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+        {
+            Console.WriteLine($"ERROR Activity caught TaskSchedulerOnUnobservedTaskException: {unobservedTaskExceptionEventArgs.Exception}");
+
+
+        }
+        private void AndroidEnvironment_UnhandledExceptionRaiser(object sender, RaiseThrowableEventArgs e)
+        {
+            Console.WriteLine($"ERROR Activity caught UnhandledException: {e.Exception}");
+;
+        }
+        #endregion
+    }
+}
---
+++ PianistApp/PianistApp/PianistApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PianistApp
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public ICommand TestCommand { get; set; }
+
+        #region Items   
+        public static readonly BindableProperty ItemsProperty =
+          BindableProperty.Create("Items", typeof(IList<string>), typeof(MainPage), null, propertyChanged: OnItemsChanged);
+
+        public IList<string> Items
+        {
+            get { return (IList<string>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        static void OnItemsChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            // Property changed implementation goes here
+        }
+        #endregion
+
+
+        public MainPage()
+        {
+            Items = new List<string>() { "One", "Two", "Three", "Four", "Five" };
+            TestCommand = new Command<string>(s => 
+            { 
+            });
+            InitializeComponent();
+        }
+    }
+}
